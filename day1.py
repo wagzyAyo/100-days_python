0fea2bf27571881c03ff89c69664904cf11caf16
wagzyAyo <talktojmcvibes@gmail.com>
2023-08-09 13:35:38 -0700

    100 days python challenge

---
+++ day1.py
@@ -0,0 +1,9 @@
+# Welcome users
+print("Hi welcome to band name suggestion matrix")
+# Prompt the user to enter the city they grow up in
+city = input("Which city did you grow up? \n")
+# Prompt the user to enter the name of their pet
+pet = input("What is the name of your pet?\n")
+# print out suggested names for the users
+print("Your band name could be " + city + " " + pet)
+# prompt should be entered in new line from the question
