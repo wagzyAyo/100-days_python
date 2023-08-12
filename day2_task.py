1904e059e57d59f7ca72ea7ed319e8c2f35ef143
wagzyAyo <talktojmcvibes@gmail.com>
2023-08-12 03:08:30 -0700

    task

--- day2_task.py
+++ day2_task.py
@@ -3,5 +3,15 @@ weight = input(
 height = input("What is your height in (meters): ")
 weight = int(weight)
 height = int(height)
-BMI = weight / height ** 2
-print("Your BMI is {}".format(BMI))
+BMI = weight / height ** height
+if BMI <= 18.5:
+    w = "underweigth"
+if BMI > 18.5:
+    w = "normal"
+if BMI > 25 and BMI < 30:
+    w = "overweight"
+if BMI > 30 and BMI < 35:
+    w = "obese"
+if BMI > 35:
+    w = "chronically obese"
+print("Your BMI is {},you are {}".format(BMI,w))
