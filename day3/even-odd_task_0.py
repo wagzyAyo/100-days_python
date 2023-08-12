9a7748bbbdebde1e8d82f39861597e767c66c904
wagzyAyo <talktojmcvibes@gmail.com>
2023-08-12 10:59:32 -0700

    even -odd

---
+++ day3/even-odd_task_0.py
@@ -0,0 +1,6 @@
+number = input("Which number do you want to check?\n")
+number = int(number)
+if number % 2 == 0:
+    print(f"The {number} is a even number")
+else:
+    print(f"The {number} is a odd number")
