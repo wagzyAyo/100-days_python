66377007eda1439dc42a1168618e40b6c9518947
wagzyAyo <talktojmcvibes@gmail.com>
2023-08-18 05:58:51 -0700

    highest and lowest

--- day5/task2.py
+++ day5/task2.py
@@ -3,7 +3,13 @@ for n in range(0, len(student_score)):
     student_score[n] = int(student_score[n])
 print(student_score)
 highest_score = 0
+lowest_score = 10000
 for score in student_score:
     if score > highest_score:
         highest_score = score
-print(highest_score)
+print(f"The highest score in the class is {highest_score}")
+
+for score in student_score:
+    if score < lowest_score:
+        lowest_score = score
+print(f"The highest score in the class is {lowest_score}")
