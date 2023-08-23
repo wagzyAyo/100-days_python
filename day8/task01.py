921bccb4dca90807ef8331720ce85cbd9a3605ab
wagzyAyo <talktojmcvibes@gmail.com>
2023-08-23 11:15:00 -0700

    task1

---
+++ day8/task01.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+import math
+def paint_cal(height, width, coverage):
+    int(height, width, coverage)
+    result = (height * width) / coverage
+    result = math.floor(result)
+    print{f"You will need {result} of paint)
