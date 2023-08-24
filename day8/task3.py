d250c690d57b3498022f705286d2cbbb9956f001
wagzyAyo <talktojmcvibes@gmail.com>
2023-08-24 10:10:42 -0700

    task3

---
+++ day8/task3.py
@@ -0,0 +1,33 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+            'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+            'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+direction = input("Type 'encode' to encrypt, type 'decode' to decode:\n")
+text = input("Type your message: \n").lower()
+shift = int(input("Type the shift number:\n"))
+
+
+def encrypt(plain_text, shift_amount):
+    coded_text = ""
+    for x in plain_text:
+        position = alphabet.index(x)
+        new_position = position + shift_amount
+        new_letter = alphabet[new_position]
+        coded_text += new_letter
+    print(f"The encoded text is {coded_text}")
+
+
+def decrypt(plain_text, shift_amount):
+    coded_text = ""
+    for x in plain_text:
+        position = alphabet.index(x)
+        new_position = position - shift_amount
+        new_letter = alphabet[new_position]
+        coded_text += new_letter
+    print(f"The decode text is {coded_text}")
+
+
+if direction == "encode":
+    encrypt(plain_text=text, shift_amount=shift)
+elif direction == "decode":
+    decrypt(plain_text=text, shift_amount=shift)
+
