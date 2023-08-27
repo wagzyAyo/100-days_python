6ed3f4855e571723cfd69c04c5ebf78ab47800de
wagzyAyo <talktojmcvibes@gmail.com>
2023-08-27 12:59:16 -0700

    day10

--- day10/day10.py
+++ day10/day10.py
@@ -35,6 +35,23 @@ symbol_op = input("pick an operation from the line above: ")
 num2 = int(input("What is your second number: "))
 
 
+def ret():
+    return
+
+
 calc_op = operations[symbol_op]
-answer = calc_op(num1, num2)
-print(f"{num1} {symbol_op} {num2} = {answer}")
+answer_1 = calc_op(num1, num2)
+print(f"{num1} {symbol_op} {num2} = {answer_1}")
+continuety = True
+
+while continuety:
+    cont = input("do you want to continue 'y' to continue 'n' for NO: ")
+    if cont == "n":
+        continuety = False
+        ret()
+    symbol_op = input("pick an operation : ")
+    calc_op2 = operations[symbol_op] 
+    num3 = int(input("Enter another number: "))
+    answer_2 = calc_op2(answer_1, num3)
+    print(f"{answer_1} {symbol_op} {num3} = {answer_2}")
+    answer_1 = answer_2
