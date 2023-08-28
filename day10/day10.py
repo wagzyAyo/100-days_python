61c488a8a488ff3c3448087d3b8ac2119f8fd7bc
wagzyAyo <talktojmcvibes@gmail.com>
2023-08-28 05:36:38 -0700

    day10

--- day10/day10.py
+++ day10/day10.py
@@ -28,11 +28,11 @@ operations = {
     "/": divide,
     "*": multiply
 }
-num1 = int(input("What is your first number: "))
+num1 = float(input("What is your first number: "))
 for op in operations:
     print(op)
 symbol_op = input("pick an operation from the line above: ")
-num2 = int(input("What is your second number: "))
+num2 = float(input("What is your second number: "))
 
 
 def ret():
@@ -45,13 +45,14 @@ print(f"{num1} {symbol_op} {num2} = {answer_1}")
 continuety = True
 
 while continuety:
-    cont = input("do you want to continue 'y' to continue 'n' for NO: ")
+    cont = input(
+        f"do you want to continue with {answer_1} 'y' to continue 'n' for NO: ")
     if cont == "n":
         continuety = False
         ret()
     symbol_op = input("pick an operation : ")
-    calc_op2 = operations[symbol_op] 
-    num3 = int(input("Enter another number: "))
+    calc_op2 = operations[symbol_op]
+    num3 = float(input("Enter another number: "))
     answer_2 = calc_op2(answer_1, num3)
     print(f"{answer_1} {symbol_op} {num3} = {answer_2}")
     answer_1 = answer_2
