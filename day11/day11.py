3fd7e2e24b7b622ce69e963cc41051f00826de7f
wagzyAyo <talktojmcvibes@gmail.com>
2023-08-28 08:27:38 -0700

    day11

--- day11/day11.py
+++ day11/day11.py
@@ -14,6 +14,12 @@ for _ in range(2):
     computer_card.append(deal_card())
 
 def calculate_score(cards):
+    if sum(cards) == 21 and len(cards) == 2:
+        return 0
+
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)
     return sum(cards)
 
 
