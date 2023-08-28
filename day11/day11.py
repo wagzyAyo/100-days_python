90d2e3a389b673e4311e649c9ca68b40aa6be4da
wagzyAyo <talktojmcvibes@gmail.com>
2023-08-28 07:51:08 -0700

    blackjack

---
+++ day11/day11.py
@@ -0,0 +1,17 @@
+import random
+cards = [11,2,3,4,5,6,7,8,9,10,10,10,10]
+
+def deal_card():
+    cards = [11,2,3,4,5,6,7,8,9,10,10,10,10]
+    card = random.choice(cards)
+    return card
+
+user_card = []
+computer_card = []
+
+for _ in range(2):
+    user_card.append(deal_card())
+    computer_card.append(deal_card())
+
+
+
