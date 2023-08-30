95b9e856710792eb8cb910d47ea3ad21e5ddd132
wagzyAyo <talktojmcvibes@gmail.com>
2023-08-30 11:39:58 -0700

    debug3

---
+++ day13/debug3.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+year = int(input("Enter the year: "))
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("It is a leap year")
+        else:
+            print("Not a leap year")
+    else:
+        print("It is a leap year")
+else:
+    print("Not a leap year")
