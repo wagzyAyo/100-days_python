1f3d2d58ab7c0c1b44b67c9e3bda1901067c37d3
wagzyAyo <talktojmcvibes@gmail.com>
2023-08-30 11:10:43 -0700

    debug1

---
+++ day13/debug1.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+number = int(input("Enter your number: "))
+if number % 2 == 0:
+    print("{} is even number".format(number))
+else:
+    print("{} is odd number".format(number))
