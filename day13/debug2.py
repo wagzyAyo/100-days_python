db14eed561015d006c687e576a1ec93161663d32
wagzyAyo <talktojmcvibes@gmail.com>
2023-08-30 11:28:52 -0700

    debug2

--- day13/debug2.py
+++ day13/debug2.py
@@ -1,13 +1,10 @@
-#!/usr/bin/python3
-# Fizz buzz debug
-number = range(1 -101)
-for x in number:
-    if x % 3 == 0 and number % 5 == 0:
+#!usr/bin/python3
+for number in range(1, 101):
+    if number % 3 == 0 and number % 5 == 0:
         print("Fizz Buzz")
-    elif x % 3 == 0:
+    elif number % 3 == 0:
         print("Fizz")
-    elif x % 5 == 0:
+    elif number % 5 == 0:
         print("Buzz")
     else:
-        print(x)
-
+        print(number)
