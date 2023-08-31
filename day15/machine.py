e4b93c5771c82bf644acd3992ab546bba8bcaaae
wagzyAyo <talktojmcvibes@gmail.com>
2023-08-31 11:04:17 -0700

    machine

---
+++ day15/machine.py
@@ -0,0 +1,34 @@
+MENU = {
+    "expresso":
+    {
+        "ingridient":{
+            "water": 50,
+            "coffe": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte":
+    {
+        "ingridient":{
+            "water": 200,
+            "milk": 150,
+            "coffe": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino":
+    {
+        "ingridient":{
+            "water": 250,
+            "milk": 100,
+            "coffe": 24,
+        },
+        "cost": 3.0,
+    },
+}
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffe": 100,
+    "money": 0
+}
