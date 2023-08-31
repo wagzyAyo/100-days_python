57728487ee58794726a32b6db707c9aef00afab1
wagzyAyo <talktojmcvibes@gmail.com>
2023-08-31 11:05:37 -0700

    main

---
+++ day15/main.py
@@ -0,0 +1,23 @@
+from machine import MENU,resources
+water = resources["water"]
+milk = resources["milk"]
+coffe = resources["coffe"]
+money = resources["money"]
+#Print Report
+order = input("What would you like? ").lower()
+if order == 'report':
+    print("")
+
+#Check if resources is sufficient?
+
+
+
+#Process coins
+
+
+
+#Check if transaction is successful
+
+
+
+#Make Coffe
