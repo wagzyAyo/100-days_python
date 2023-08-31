f03e84e10b2c847015c14162122a3fff1fffe31f
wagzyAyo <talktojmcvibes@gmail.com>
2023-08-31 05:21:47 -0700

    list

---
+++ day14/name.py
@@ -0,0 +1,69 @@
+data = [
+    {
+        'name': 'Instagram',
+        'followers_count': 651,
+        'description': 'social media platform',
+        'country': 'United States'
+    },
+    {
+        'name': 'Cristiano Ronaldo',
+        'followers_count': 602,
+        'description': 'Footballer',
+        'country': 'Portugal'
+    },
+    {
+        'name': 'Ariana Grande',
+        'followers_count': 378,
+        'description': 'Musician and Actress',
+        'country': 'United States'
+    },
+    {
+        'name': 'Dwayne Johson',
+        'followers_count': 389,
+        'description': 'Actor and Professional wrestler',
+        'country': 'United States'
+    },
+    {
+        'name': 'Lionel Messi',
+        'followers_count': 485,
+        'description': 'Footabller',
+        'country': 'Argentina'
+    },
+    {
+        'name': 'Selena Gomes',
+        'followers_count': 428,
+        'description': 'Musician',
+        'country': 'United States'
+    },
+    {
+        'name': 'Kylie jenner',
+        'followers_count': 301,
+        'description': 'Musician',
+        'country': 'United States'
+    },
+    {
+        'name': 'Kim Kardashian',
+        'followers_count': 363,
+        'description': 'Television personality, model and businesswoman',
+        'country': 'United States'
+    },
+    {
+        'name': 'Beyonce',
+        'followers_count': 316,
+        'description': 'Musician',
+        'country': 'United States'
+    },
+    {
+        'name': 'Kloe Kardashian',
+        'followers_count': 311,
+        'description': 'Television personality and model',
+        'country': 'United States'
+    },
+    {
+        'name': 'Nike',
+        'followers_count': 304,
+        'description': 'Sportswear multinational',
+        'country': 'United States'
+    },
+]
+
