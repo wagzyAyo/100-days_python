1fb22895e1598b2b6ec95fb5f197035241329532
wagzyAyo <talktojmcvibes@gmail.com>
2023-09-03 03:22:20 -0700

    init

--- day17/class.py
+++ day17/class.py
@@ -2,10 +2,19 @@ class User:
     def __init__(self, user_id, username):
         self.id = user_id
         self.name = username
-        self.follower = 0
+        self.followers = 0
+        self.following = 0
+
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
 
 
 # Naming a class in python all first word start with capital PASCAL CASE
 user_1 = User('001', 'Ayomide')
-print(user_1.name)
-
+user_2 = User("002", "Angela")
+user_1.follow(user_2)
+print(user_1.followers)
+print(user_1.following)
+print(user_2.followers)
+print(user_2.following)
