dcae6d84146e5a6754adf66c85e5b2d9262435ab
ayo <talktojmcvibes@gmail.com>
2023-09-05 15:02:36 -0700

    challenge 3

--- day18/main.py
+++ day18/main.py
@@ -3,20 +3,21 @@ import random
 
 timmy = Turtle()
 timmy.shape("turtle")
-timmy.color("orange")
 
 colors = ["turquoise", "medium blue", "dark slate gray",
           "light steel blue", "dark goldenrod", "slate blue"]
 
 
-direction = [0, 90, 180, 270]
-timmy.width(15)
-timmy.speed(0)
+def random_colors():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    colors = (r, g, b)
+    return colors
 
-for _ in range(200):
-    timmy.color(random.choice(colors))
-    timmy.forward(30)
-    timmy.setheading(random.choice(direction))
+
+timmy.circle(100)
+timmy.speed(0)
 
 
 screen = Screen()
