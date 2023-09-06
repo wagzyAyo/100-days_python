c8b5c28e2018d66d4797a22e1294a989211731f0
ayo <talktojmcvibes@gmail.com>
2023-09-06 11:17:42 -0700

    snake day 20

--- day20/snake.py
+++ day20/snake.py
@@ -1,6 +1,11 @@
 from turtle import Turtle
 
 INITIAL_POSITION = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 
 class Snake:
@@ -8,6 +13,7 @@ class Snake:
     def __init__(self):
         self.segment = []
         self.create_snake()
+        self.snake_head = self.segment[0]
 
     def create_snake(self):
         for position in INITIAL_POSITION:
@@ -23,4 +29,20 @@ class Snake:
             new_x = self.segment[seg_num - 1].xcor()
             new_y = self.segment[seg_num - 1].ycor()
             self.segment[seg_num].goto(new_x, new_y)
-        self.segment[0].forward(20)
+        self.segment[0].forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.snake_head.heading() != DOWN:
+            self.snake_head.setheading(UP)
+
+    def down(self):
+        if self.snake_head.heading() != UP:
+            self.snake_head.setheading(DOWN)
+
+    def left(self):
+        if self.snake_head.heading() != RIGHT:
+            self.snake_head.setheading(LEFT)
+
+    def right(self):
+        if self.snake_head.heading() != LEFT:
+            self.snake_head.setheading(RIGHT)
