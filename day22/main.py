d293576cc8a9cad1288608eb42a030ec7cdec69d
ayo <talktojmcvibes@gmail.com>
2023-09-08 14:02:43 -0700

    collision with rght and left paddle

--- day22/main.py
+++ day22/main.py
@@ -28,7 +28,18 @@ while game_is_on:
 
     # Detect collisin with wall
     if ball.ycor() > 280 or ball.ycor() < -280:
-        ball.bounce()
+        ball.bounce_y()
 
+    # Detect collision with right paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    # Detect when right paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+
+    # Detect when right paddle misses
+    if ball.xcor() < - 380:
+        ball.reset_position()
 
 screen.exitonclick()
