2a39249b25628da93aa3079730fa774de793fd4e
ayo <talktojmcvibes@gmail.com>
2023-09-08 13:05:30 -0700

    paddle

--- day22/main.py
+++ day22/main.py
@@ -1,9 +1,25 @@
 from turtle import Turtle, Screen
+from paddle import Paddle
 
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 
 screen = Screen()
+screen.tracer(0)
+
+
 screen.bgcolor("black")
 screen.setup(height=600, width=800)
+screen.title("Pong")
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
 
 
 screen.exitonclick()
