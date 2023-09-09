2c8519214d8e9cddd3c4e49992451d5fad7735a1
ayo <talktojmcvibes@gmail.com>
2023-09-09 10:26:39 -0700

    get to finish line

--- day23/main.py
+++ day23/main.py
@@ -25,3 +25,10 @@ while game_is_on:
     for car in car_manager.all_cars:
         if car.distance(player) < 20:
             game_is_on = False
+
+    # Detect succesful crossing
+    if player.is_at_finish_line():
+        player.go_to_start()
+
+
+screen.exitonclick()
