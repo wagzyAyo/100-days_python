f152a9dcc4fd8f4f6895f39d17b1936ed62fe197
ayo <talktojmcvibes@gmail.com>
2023-09-14 09:05:08 -0700

    update tkinter

--- day27/main.py
+++ day27/main.py
@@ -1,12 +1,21 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("My First GUI")
 window.minsize(width=500, height=300)
 
 
-my_label = tkinter.Label(text="I am a label", font=("Arial", 24, "bold"))
+my_label = Label(text="I am a label", font=("Arial", 24, "bold"))
 my_label.pack()
 
+my_label.config(text="New text")
+
+
+def clicked_button():
+    print("I got clicked")
+
+
+button = Button(text="click me", command=clicked_button)
+button.pack()
 
 window.mainloop()
