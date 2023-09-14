2deaa1807935f0003302e674b3f5a9b1645e1d60
ayo <talktojmcvibes@gmail.com>
2023-09-14 08:26:42 -0700

    tkinter

---
+++ day27/main.py
@@ -0,0 +1,12 @@
+import tkinter
+
+window = tkinter.Tk()
+window.title("My First GUI")
+window.minsize(width=500, height=300)
+
+
+my_label = tkinter.Label(text="I am a label", font=("Arial", 24, "bold"))
+my_label.pack()
+
+
+window.mainloop()
