e3e082f61ca08a15ed2ff0d3d81d2cdf342ab3ca
ayo <talktojmcvibes@gmail.com>
2023-09-16 11:27:33 -0700

    ve function

--- day29/main.py
+++ day29/main.py
@@ -1,5 +1,14 @@
 from tkinter import *
 
+
+def save():
+    web = web_input.get()
+    mail = mail_input.get()
+    psw = psw_input.get()
+    with open("data.text", mode="a") as file:
+        file.write(f"{web} | {mail} | {psw}")
+
+
 FONT_NAME = "Courier"
 
 
@@ -18,12 +27,14 @@ web_label = Label(text="Website:", font=(FONT_NAME))
 web_label.grid(row=1, column=0)
 web_input = Entry(width=35)
 web_input.grid(row=1, column=1, columnspan=2)
+web_input.focus()
 
 
 mail_label = Label(text="Email/username:", font=(FONT_NAME))
 mail_label.grid(row=2, column=0)
 mail_input = Entry(width=35)
 mail_input.grid(row=2, column=1, columnspan=2)
+mail_input.insert(0, "talktoayo@hmail.com")
 
 
 psw_label = Label(text="password:", font=(FONT_NAME))
@@ -34,7 +45,7 @@ psw_input.grid(row=3, column=1)
 gen_btn = Button(text="Generate password")
 gen_btn.grid(row=3, column=2)
 
-add_btn = Button(text="Add", width=36)
+add_btn = Button(text="Add", width=36, command=save)
 add_btn.grid(row=4, column=1, columnspan=2)
 
 
