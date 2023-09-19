3b2fa199ce0a0e428106cfbd74901d1ba9b6bfc5
ayo <talktojmcvibes@gmail.com>
2023-09-19 09:06:16 -0700

    flip cards

--- day31/main.py
+++ day31/main.py
@@ -7,23 +7,36 @@ BACKGROUND_COLOR = "#B1DDC6"
 
 data = pandas.read_csv("./day31/data/words.csv")
 to_learn = data.to_dict(orient="records")
+current_card = {}
 
 
 def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
     current_card = random.choice(to_learn)
-    print(current_card["French"])
-    canvas.itemconfig(card_title, text="French")
-    canvas.itemconfig(card_word, text=current_card["French"])
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    canvas.itemconfig(card_background, image=image1)
+    flip_timer = window.after(3000, func=flip_card)
+
+
+def flip_card():
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_background, image=image2)
 
 
 window = Tk()
 window.title("Flashy")
 window.config(padx=30, pady=30, bg=BACKGROUND_COLOR)
 
+flip_timer = window.after(3000, func=flip_card)
 canvas = Canvas(width=600, height=404,
                 bg=BACKGROUND_COLOR, highlightthickness=0)
 image1 = PhotoImage(file="./day31/images/Rectangle 3.png")
-canvas.create_image(300, 204, image=image1)
+card_background = canvas.create_image(300, 204, image=image1)
+# Back image
+image2 = PhotoImage(file="./day31/images/Rectangle 4.png")
 
 card_title = canvas.create_text(
     300, 155, text="", font=("Ariel", 20, "italic"))
