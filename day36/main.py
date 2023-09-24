01ee5414671830419277f15b11f937df5e791d64
ayo <talktojmcvibes@gmail.com>
2023-09-24 02:45:01 -0700

    difference

--- day36/main.py
+++ day36/main.py
@@ -18,4 +18,16 @@ data = requests.get(STOCK_ENDPOINT, params=parameters)
 data.raise_for_status()
 stock_data = data.json()["Time Series (Daily)"]
 data_list = [value for (key, value) in stock_data.items()]
-print(data_list)
+yesterday_data = data_list[0]
+yesterday_closing_price = yesterday_data["4. close"]
+print(yesterday_closing_price)
+
+
+day_before_yesterday = data_list[1]
+day_before_yesterday_close = day_before_yesterday["4. close"]
+print(day_before_yesterday_close)
+
+difference_in_stock_price = float(
+    day_before_yesterday_close) - float(yesterday_closing_price)
+
+print(difference_in_stock_price)
