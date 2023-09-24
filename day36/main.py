f7158e2628cb00181baaf7192866c0a3bc8f8898
ayo <talktojmcvibes@gmail.com>
2023-09-24 03:26:26 -0700

    news api

--- day36/main.py
+++ day36/main.py
@@ -6,7 +6,8 @@ COMPANY_NAME = "Tesla Inc"
 
 API_KEY = "H1R6T6GE7NUG1FHC"
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
-NEWS_ENDPOINT = "https://newsapi.org/v2/everyrhing"
+NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
+NEWS_API_KEY = "94388d43ec994d8b899f4444156e73a3"
 
 parameters = {
     "function": "TIME_SERIES_DAILY",
@@ -27,7 +28,23 @@ day_before_yesterday = data_list[1]
 day_before_yesterday_close = day_before_yesterday["4. close"]
 print(day_before_yesterday_close)
 
-difference_in_stock_price = float(
-    day_before_yesterday_close) - float(yesterday_closing_price)
+difference_in_stock_price = abs(float(
+    yesterday_closing_price) - float(day_before_yesterday_close))
 
 print(difference_in_stock_price)
+
+diff_percent = (difference_in_stock_price /
+                float(day_before_yesterday_close)) * 100
+print(diff_percent)
+
+
+new_params = {
+    "apiKey": NEWS_API_KEY,
+    "qInTitle": COMPANY_NAME,
+}
+if diff_percent > 3:
+    response = requests.get(NEWS_ENDPOINT, params=new_params)
+    response.raise_for_status()
+    new_data = response.json()["articles"]
+    three_articles = new_data[:3]
+    print(three_articles)
