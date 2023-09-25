987afcf733de990ca90a733764e1a87870d7de71
ayo <talktojmcvibes@gmail.com>
2023-09-25 03:18:57 -0700

    post

--- day37/main.py
+++ day37/main.py
@@ -1,4 +1,5 @@
 import requests
+from datetime import datetime
 
 TOKEN = "giohieiq1903kdjjhdq1"
 USER_NAME = "ayo79"
@@ -34,12 +35,16 @@ Headers = {
 # response = requests.post(graph_endpoint, json=graph_config, headers=Headers)
 # print(response.text)
 
+today = datetime(year=2023, month=8, day=7)
+
+
 pixel_endpoint = f"{pixella_endpoint}/{USER_NAME}/graphs/{GRAPH1}"
 
 pixel_params = {
-    "date": "20230805",
-    "quantity": "80.9"
+    "date": today.strftime("%Y%m%d"),
+    "quantity": "45.9"
 }
+print(pixel_params["date"])
 
 response = requests.post(
     url=pixel_endpoint, json=pixel_params, headers=Headers)
