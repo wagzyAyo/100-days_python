34c421e4c002929fa16ae9f6fdb5d1c0bac3a11d
ayo <talktojmcvibes@gmail.com>
2023-09-25 03:07:46 -0700

    success

--- day37/main.py
+++ day37/main.py
@@ -2,6 +2,8 @@ import requests
 
 TOKEN = "giohieiq1903kdjjhdq1"
 USER_NAME = "ayo79"
+GRAPH1 = "graph1"
+
 
 pixella_endpoint = "https://pixe.la/v1/users"
 
@@ -29,5 +31,16 @@ graph_config = {
 Headers = {
     "X-USER-TOKEN": TOKEN
 }
-response = requests.post(graph_endpoint, json=graph_config, headers=Headers)
+# response = requests.post(graph_endpoint, json=graph_config, headers=Headers)
+# print(response.text)
+
+pixel_endpoint = f"{pixella_endpoint}/{USER_NAME}/graphs/{GRAPH1}"
+
+pixel_params = {
+    "date": "20230805",
+    "quantity": "80.9"
+}
+
+response = requests.post(
+    url=pixel_endpoint, json=pixel_params, headers=Headers)
 print(response.text)
