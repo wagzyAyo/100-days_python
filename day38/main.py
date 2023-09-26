34aa383d5a0e78baa815273835140b89e08b9c72
ayo <talktojmcvibes@gmail.com>
2023-09-26 12:56:25 -0700

    step 4

--- day38/main.py
+++ day38/main.py
@@ -14,7 +14,7 @@ SHEETY_ENDPOINT = "https://api.sheety.co"
 
 API_END_POINT = "https://trackapi.nutritionix.com/v2/natural/exercise"
 
-msg = input("Tell me which excercise you did? ")
+msg = input("Tell me which excercise you did? ").title()
 
 exercise_parameters = {
     "query": msg,
@@ -24,4 +24,27 @@ exercise_parameters = {
 response = requests.post(
     url=API_END_POINT, json=exercise_parameters, headers=HEADERS)
 result = response.json()
+
+length = len(result['exercises'])
+print(length)
+
 print(result)
+
+new_data = []
+
+
+for item, value in result.items():
+    count = 0
+    if count > len(result):
+        break
+
+    user_input = result[item][count]
+    user_activity = user_input["user_input"]
+    work_out_duration = user_input['duration_min']
+    calories_burn = user_input['nf_calories']
+    count + 1
+    date = datetime.now().strftime("%d/%m/%Y")
+    new_data.append([date, user_activity, work_out_duration, calories_burn])
+
+
+print(new_data)
