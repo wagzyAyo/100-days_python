3deeb7edab5b1334d77955711d6e8492edd63261
ayo <talktojmcvibes@gmail.com>
2023-09-30 00:44:52 -0700

    kiwi api

--- day39/main.py
+++ day39/main.py
@@ -1,18 +1,32 @@
-from data_manager import DataManager
 from flight_search import FlightSearch
+from data_manager import DataManager
+from datetime import datetime, timedelta
 # This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+
+ORIGIN_CITY_IATA = "LON"
 
 
 data_manager = DataManager()
 sheet_data = data_manager.get_destination_data()
 
+flight_search = FlightSearch()
 
 if sheet_data[0]["iataCode"] == "":
-    from flight_search import FlightSearch
-    flight_search = FlightSearch()
+
     for row in sheet_data:
         row["iataCode"] = flight_search.get_destination_code(row["city"])
     print(f"sheet_data:\n {sheet_data}")
 
     data_manager.destination_data = sheet_data
     data_manager.update_destination_codes()
+
+tomorrow = datetime.now() + timedelta(days=1)
+six_month_from_today = datetime.now() + timedelta(days=(6 * 30))
+
+for destination in sheet_data:
+    flight = flight_search.check_flights(
+        ORIGIN_CITY_IATA,
+        destination["iataCode"],
+        from_time=tomorrow,
+        to_time=six_month_from_today
+    )
