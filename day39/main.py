d7cd330d6c86f1e5c031fef5d806d747a11d1ee6
ayo <talktojmcvibes@gmail.com>
2023-09-30 00:58:06 -0700

    twilio

--- day39/main.py
+++ day39/main.py
@@ -1,15 +1,17 @@
 from flight_search import FlightSearch
 from data_manager import DataManager
 from datetime import datetime, timedelta
+from notification_manager import NotificationManager
 # This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
 
 ORIGIN_CITY_IATA = "LON"
+notification_manager = NotificationManager()
+flight_search = FlightSearch()
 
 
 data_manager = DataManager()
 sheet_data = data_manager.get_destination_data()
 
-flight_search = FlightSearch()
 
 if sheet_data[0]["iataCode"] == "":
 
@@ -30,3 +32,8 @@ for destination in sheet_data:
         from_time=tomorrow,
         to_time=six_month_from_today
     )
+
+    if flight.price < destination["lowestPrice"]:
+        notification_manager.send_sms(
+            message=f"Low price alert! Only £{flight.price} to fly from {flight.origin_city}-{flight.origin_airport} to {flight.destination_city}-{flight.destination_airport}, from {flight.out_date} to {flight.return_date}."
+        )
