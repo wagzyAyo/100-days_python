86eb090e919d5a863e2e9aac743d271851e7d20a
wagzyAyo <talktojmcvibes@gmail.com>
2023-10-04 06:04:28 -0700

    bs4

--- day 45/main.py
+++ day 45/main.py
@@ -11,3 +11,17 @@ soup = BeautifulSoup(contents, "lxml")
 # print(soup.title)
 # print(soup.ul)
 # print(soup.li)
+
+tag_p = soup.find_all(name="p")
+print(tag_p)
+
+for tag in tag_p:
+    print(tag.getText())
+    print(tag.get("href"))
+
+
+header = soup.find(name="h1", id="name")
+print(header)
+
+projects = soup.find(name="h3", class_="projects")
+print(projects)
