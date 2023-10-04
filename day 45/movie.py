f96458a0ab4128af49859105dd8cad0e5c1923e9
wagzyAyo <talktojmcvibes@gmail.com>
2023-10-04 12:02:29 -0700

    day 45

--- day 45/movie.py
+++ day 45/movie.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import lxml
+import re
 
 empire_site = requests.get(
     url="https://www.empireonline.com/movies/features/best-movies-2/")
@@ -13,4 +14,11 @@ title = soup.select(
 
 film_title = [text.getText() for text in title]
 
-print(film_title)
+# print(film_title)
+
+number = 1
+for film in film_title:
+    splited = film.split(')')
+    movie_title = splited[1]
+    print(f"{number}) {movie_title}")
+    number += 1
