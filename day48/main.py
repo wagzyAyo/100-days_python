811e5dc8be549e7076ec7e9a499d244fa69b2bad
wagzyAyo <talktojmcvibes@gmail.com>
2023-10-12 06:51:35 -0700

    price

--- day48/main.py
+++ day48/main.py
@@ -3,15 +3,17 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-
 chrome_driver_path = "C:\Development\chromedriver.exe"
 
 driver = webdriver.Chrome()
 
 driver.get("https://coinmarketcap.com/currencies/shiba-inu/")
 
-price = driver.find_element(By.CSS_SELECTOR, ".sc-16891c57-0 ksCNvw base-text")
+# Use an explicit wait to wait for the element to be present
+wait = WebDriverWait(driver, 10)  # Adjust the timeout as needed
+price = wait.until(EC.presence_of_element_located(
+    (By.CSS_SELECTOR, ".sc-16891c57-0.ksCNvw.base-text")))
+
 print(price.text)
 
-# driver.close()
 driver.quit()
