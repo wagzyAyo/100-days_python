c660c7b0232a431160e05051024bd729b8705274
wagzyAyo <talktojmcvibes@gmail.com>
2023-10-12 06:45:56 -0700

    webdriver

--- day48/main.py
+++ day48/main.py
@@ -1,12 +1,16 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 
 chrome_driver_path = "C:\Development\chromedriver.exe"
 
 driver = webdriver.Chrome()
 
-driver.get("https://www.amazon.com/Beelink-SER5-Computer-Display-Bluetooth/dp/B0C3VJGWSL/ref=sr_1_1_sspa?crid=G9S4AW54HEY2&keywords=pc&qid=1696601812&sprefix=%2Caps%2C569&sr=8-1-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&psc=1&smid=A3OHI25ZCHSGMQ")
+driver.get("https://coinmarketcap.com/currencies/shiba-inu/")
 
-price = driver.find_element("a-offscreen")
+price = driver.find_element(By.CSS_SELECTOR, ".sc-16891c57-0 ksCNvw base-text")
 print(price.text)
 
 # driver.close()
