b96ad1b447388eaf0c9699e99366f4d2b893075a
wagzyAyo <talktojmcvibes@gmail.com>
2023-10-12 07:21:18 -0700

    main.py

--- day49/main.py
+++ day49/main.py
@@ -12,7 +12,7 @@ wait = WebDriverWait(driver, 10)
 driver.get("https://www.amazon.com/Beelink-SER5-Computer-Display-Bluetooth/dp/B0C3VJGWSL/ref=sr_1_1_sspa?crid=G9S4AW54HEY2&keywords=pc&qid=1696601812&sprefix=%2Caps%2C569&sr=8-1-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&psc=1&smid=A3OHI25ZCHSGMQ")
 
 price = wait.until(EC.presence_of_element_located(
-    driver.find_element(By.CSS_SELECTOR, ".offscreen")))
+    (By.CSS_SELECTOR, ".a-offscreen")))
 
 print(price.text)
 
