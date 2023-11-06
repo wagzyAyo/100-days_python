7dd25ab853998003ebf7144fc81146479c405296
wagzyAyo <talktojmcvibes@gmail.com>
2023-11-06 12:00:19 -0800

    decorator

--- Backend with python/app/main.py
+++ Backend with python/app/main.py
@@ -3,12 +3,33 @@ from flask import Flask
 app = Flask(__name__)
 
 
+def make_bold(function):
+    def wrapper():
+        return "<b>" + function() + "</b>"
+    return wrapper
+
+
+def make_emphasis(function):
+    def wrapper():
+        return "<em>" + function() + "</em>"
+    return wrapper
+
+
+def make_underline(function):
+    def wrapper():
+        return "<u>" + function() + "</u>"
+    return wrapper
+
+
 @app.route("/")
 def home():
     return "Hello World"
 
 
 @app.route("/bye")
+@make_bold
+@make_emphasis
+@make_underline
 def bye():
     return "Bye!"
 
