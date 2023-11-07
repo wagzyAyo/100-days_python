64ee945b79a10bad74faeebfbea0c5143fcc41b8
wagzyAyo <talktojmcvibes@gmail.com>
2023-11-06 19:21:10 -0800

    alt

--- Backend with python/class/excercise.py
+++ Backend with python/class/excercise.py
@@ -1,4 +1,7 @@
+import time
 # Create the logging_decorator() function 👇
+
+
 def logging_decorator(function):
     def wrapper(*args, **kwargs):
         func = function.__name__
@@ -14,4 +17,11 @@ def the_func(a, b, c):
     return a*b*c
 
 
+@logging_decorator
+def alt(x, y, v):
+    time.sleep(2)
+    return x+y+v
+
+
 the_func(2, 3, 4)
+alt(20, 10, 10)
