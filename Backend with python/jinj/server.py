b8a7525da47f612854a21d8ceaaf5674c9d0ae66
wagzyAyo <talktojmcvibes@gmail.com>
2023-11-10 01:23:57 -0800

    random

--- Backend with python/jinj/server.py
+++ Backend with python/jinj/server.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
+import random
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    number = random.randint(1, 10)
+    return render_template("index.html", rand=number)
 
 
 if __name__ == "__main__":
