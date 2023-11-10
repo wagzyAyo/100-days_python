586b701faf77baccf2de41c62d432b2f81c433f5
wagzyAyo <talktojmcvibes@gmail.com>
2023-11-10 02:17:47 -0800

    api with flas

--- Backend with python/jinj/server.py
+++ Backend with python/jinj/server.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 import random
 import datetime
+import requests
 
 app = Flask(__name__)
 date = datetime.datetime.now()
@@ -14,6 +15,20 @@ def home():
     number = random.randint(1, 10)
 
     return render_template("index.html", rand=number, this_year=year)
+
+
+@app.route("/guess/<name>")
+def guess(name):
+    gender_api = f"https://api.genderize.io?name={name}"
+    age_api = f"https://api.agify.io?name={name}"
+    gender_response = requests.get(gender_api)
+    gender_response = gender_response.json()
+    gender = gender_response["gender"]
+    age_response = requests.get(age_api)
+    age_response = age_response.json()
+    age = age_response["age"]
+
+    return render_template("guess.html", person_name=name, gender=gender, age=age)
 
 
 if __name__ == "__main__":
