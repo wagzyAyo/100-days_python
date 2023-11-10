2d2911516ed1baa31db08a16756a70722a1a24ba
wagzyAyo <talktojmcvibes@gmail.com>
2023-11-10 01:59:51 -0800

    footer not hardcoded

--- Backend with python/jinj/server.py
+++ Backend with python/jinj/server.py
@@ -1,13 +1,19 @@
 from flask import Flask, render_template
 import random
+import datetime
 
 app = Flask(__name__)
+date = datetime.datetime.now()
+date = str(date)
+
+year = date.split("-")[0]
 
 
 @app.route("/")
 def home():
     number = random.randint(1, 10)
-    return render_template("index.html", rand=number)
+
+    return render_template("index.html", rand=number, this_year=year)
 
 
 if __name__ == "__main__":
