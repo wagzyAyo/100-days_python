4b5a4bd3dd216f77567d4e4db6918497e4f004d9
wagzyAyo <talktojmcvibes@gmail.com>
2023-11-27 11:10:31 -0800

    main render

--- Backend with python/startbootstrap-clean-blog-gh-pages/main.py
+++ Backend with python/startbootstrap-clean-blog-gh-pages/main.py
@@ -6,13 +6,10 @@ app = Flask(__name__)
 end_point = "https://api.npoint.io/7b4e9e3e6a08360d6ba0"
 blog_post = requests.get(end_point).json()
 
-for post in blog_post:
-    print(post['author'])
-
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', blog_post=blog_post)
 
 
 @app.route('/about')
