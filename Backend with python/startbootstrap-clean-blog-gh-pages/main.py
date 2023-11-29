1e73b11c34c24973662d5f0593ef92571714883f
Ayo <106088806+wagzyAyo@users.noreply.github.com>
2023-11-29 11:46:14 +0100

    main.py

--- Backend with python/startbootstrap-clean-blog-gh-pages/main.py
+++ Backend with python/startbootstrap-clean-blog-gh-pages/main.py
@@ -1,25 +1,14 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template, request
 import requests
-
-app = Flask(__name__)
 
 end_point = "https://api.npoint.io/7b4e9e3e6a08360d6ba0"
 blog_post = requests.get(end_point).json()
 
+app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html', blog_post=blog_post)
-
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
+    return render_template("index.html", blog_post=blog_post)
 
 
 @app.route("/post/<int:index>")
@@ -31,15 +20,24 @@ def show_post(index):
             break
     return render_template("post.html", post=requested_post)
 
-@app.route("/form-entry", methods=["POST"])
-def receive_data():
-    data = request.form
-    print(data["name"])
-    print(data["email"])
-    print(data["phone"])
-    print(data["message"])
-    return "<h1>Successfully sent your message</h1>"
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 
-if __name__ == '__main__':
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    if request.method == "POST":
+        data = request.form
+        print(data["name"])
+        print(data["email"])
+        print(data["phone"])
+        print(data["message"])
+        return render_template('contact.html', msg_sent=True)
+    else:
+        return render_template("contact.html", msg_sent=False)
+
+
+if __name__ == "__main__":
     app.run(debug=True)
