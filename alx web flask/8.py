5fca2198ff579c229adc3b182eb25251c7febad0
Ayo <106088806+wagzyAyo@users.noreply.github.com>
2023-12-26 12:04:50 +0100

    cities htm

---
+++ alx web flask/8.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Start Flask app"""
+from flask import Flask, render_template
+import storage
+from models.state import State
+
+
+app = Flask(__name__)
+
+@app.teardown_appcontext
+def close():
+    """Close down the session"""
+    storage.close()
+
+@app.route('/states_list', strict_slashes=False)
+def state_list():
+    """States Route"""
+    states = storage.all(state)
+    return render_template('7-states_list.html', states=states)
