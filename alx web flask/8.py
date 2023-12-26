4bdd0f3cad75513bba14a53a99bdf01ed186c687
Ayo <106088806+wagzyAyo@users.noreply.github.com>
2023-12-26 12:11:16 +0100

    run app

--- alx web flask/8.py
+++ alx web flask/8.py
@@ -17,3 +17,7 @@ def state_list():
     """States Route"""
     states = storage.all(state)
     return render_template('7-states_list.html', states=states)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
